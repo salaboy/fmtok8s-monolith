3f165fa4eab33fd2e4b56cfa3427118c214c2a3a
Matheus Cruz <matheus.cruz@radixeng.com.br>
2020-09-18 16:51:01 -0300

    Removing specific ENGINE and CHARSET

--- src/main/resources/db/migration/V0001__initial_tables.sql
+++ src/main/resources/db/migration/V0001__initial_tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE `proposal` (
     email VARCHAR(200) NOT NULL,
     approved BIT NOT NULL,
     status INT(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 CREATE TABLE `agenda_item` (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
@@ -15,4 +15,4 @@ CREATE TABLE `agenda_item` (
     author VARCHAR(200) NOT NULL,
     day VARCHAR(200) NOT NULL,
     time VARCHAR(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
