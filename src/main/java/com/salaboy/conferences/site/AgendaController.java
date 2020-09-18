dc414b6a7ef5f3cb1c28c03a60cabf7c098da904
Matheus Cruz <matheus.cruz@radixeng.com.br>
2020-09-18 15:30:38 -0300

    Removing unnecessary annotation @Transactional

--- src/main/java/com/salaboy/conferences/site/AgendaController.java
+++ src/main/java/com/salaboy/conferences/site/AgendaController.java
@@ -2,14 +2,14 @@ package com.salaboy.conferences.site;
 
 import com.salaboy.conferences.site.model.AgendaItem;
 import com.salaboy.conferences.site.repository.AgendaItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
-
-import javax.transaction.Transactional;
 
 @RestController
 @RequestMapping("/agenda/")
@@ -20,7 +20,6 @@ public class AgendaController {
     private AgendaItemRepository agendaItemRepository;
 
     @PostMapping()
-    @Transactional
     public String newAgendaItem(@RequestBody AgendaItem agendaItem) {
 
         log.info("> New Agenda Item Received: " + agendaItem);
